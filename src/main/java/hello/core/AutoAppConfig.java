f06f9f6ed4a8f49727086a6ea26cdb74ddf562aa
이정헌 <aoc05230@naver.com>
2025-07-20 06:38:35 +0900

    Section8 FINISH

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.FilterType;
 //        탐색할 패키지의 시작 위치를 지정한다. 이 패키지를 포함해서 하위 패키지를 모두 탐색한다.
 //        Default값은 @ComponentScan이 붙은 설정 정보 클래스의 패키지 + 하위 패키지를 모두 탐색한다.
 //        권장하는 방법은 설정 정보 클래스의 위치를 프로젝트의 최상단에 두는 것 이다.
-        basePackages = "hello.core.member",
+//        basePackages = "hello.core.member",
 
 //         기존 AppConfig에서 만들어둔 설정 정보를 컴포넌트 스캔 대상에서 제외한다.
 //         보통 설정 정보를 컴포넌트 스캔 대상에서 제외하지는 않지만
@@ -22,8 +22,8 @@ import org.springframework.context.annotation.FilterType;
 )
 public class AutoAppConfig {
 
-    @Bean(name = "memoryMemberRepository")
-    public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
+//    @Bean(name = "memoryMemberRepository")
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
 }
